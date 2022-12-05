8843781172778801613ff89edd7fa1df055b959c
BrigliaAn <briglia.antonella@gmail.com>
2022-12-04 21:15:36 -0300

    Agrego en controller logica que devuelve el link para la busqueda

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,4 +1,32 @@
+require 'rubygems'
+require 'mechanize'
+require 'json'
+require "csv"
+
 class BookController < ApplicationController
   def index
+    agent = Mechanize.new
+    print(params)
+    if params[:type] != 'nonfic'
+      page = agent.get("https://libgen.is/#{params[:type]}/?q=#{params[:search]}")
+
+      book_link = page.at_xpath("//table[@class='catalog']//tbody//td/p/a/@href")
+    else
+      page = agent.get("https://libgen.is/search.php?req=#{params[:search]}&lg_topic=libgen&open=0&view=simple&res=25&phrase=1&column=def")
+
+      book_link = page.at_xpath("//table[@class='c']//tbody//td/a[@id]/@href")
+
+    end
+    @link= mirror_getter("https://libgen.is#{book_link}")
   end
+
+  private
+  def mirror_getter(book_link)
+    agent = Mechanize.new
+    page = agent.get(book_link)
+  
+    mirror_link = page.at_xpath("//td[contains(text(),'Download')]/..//td//a/@href")
+    return mirror_link
+  end
+
 end
